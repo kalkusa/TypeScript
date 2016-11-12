c02e1ee9cc6d6aa3631320c6012c713cdff05a44
landeeyo <landeeyo@10g.pl>
2016-11-12 18:13:57 +0100

    Added interface and class example (polymorphism).

--- Playground/app.js
+++ Playground/app.js
@@ -1,1 +1,27 @@
-console.log('Hello world');
+/**
+ * Dog
+ */
+var Dog = (function () {
+    function Dog() {
+    }
+    Dog.prototype.speak = function () {
+        console.log("Bark");
+    };
+    return Dog;
+}());
+/**
+ * Cat
+ */
+var Cat = (function () {
+    function Cat() {
+    }
+    Cat.prototype.speak = function () {
+        console.log("Meow");
+    };
+    return Cat;
+}());
+var speakable;
+speakable = new Dog();
+speakable.speak();
+speakable = new Cat();
+speakable.speak();
--- Playground/app.ts
+++ Playground/app.ts
@@ -1,1 +1,27 @@
-console.log('Hello world');
+interface ISpeakable {
+    speak(): void;
+}
+
+/**
+ * Dog
+ */
+class Dog implements ISpeakable {
+    speak(): void {
+        console.log("Bark");
+    }
+}
+
+/**
+ * Cat
+ */
+class Cat {
+    speak(): void {
+        console.log("Meow");
+    }
+}
+
+let speakable:ISpeakable;
+speakable = new Dog();
+speakable.speak();
+speakable = new Cat();
+speakable.speak();
