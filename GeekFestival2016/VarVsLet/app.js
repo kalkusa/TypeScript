b0387a10a9927be937f2011e593959f3f163a596
kalkusa <kalkus@10g.pl>
2016-11-14 15:24:52 +0100

    Added demo of comparison between var and let.

---
+++ GeekFestival2016/VarVsLet/app.js
@@ -0,0 +1,6 @@
+for (var i = 0; i < 10; i++) { }
+for (var j = 0; j < 10; j++) { }
+console.log(i);
+console.log(j);
+//Uwaga: jeśli transpilujemy do ES<6 TSC rzuci błąd, ale skompiluje do var 
+//# sourceMappingURL=app.js.map
